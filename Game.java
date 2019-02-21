92cf494623df7e3f469d2b4c519da3ff866d74b6
alhedo <47435948+alhedo@users.noreply.github.com>
2019-02-21 10:32:34 -0700

    Add files via upload
    this is the main/Game file.

---
+++ Game.java
@@ -0,0 +1,11 @@
+public class Game{
+  private final Player player = new Player; //check class.
+
+  public void play(){
+    System.out.println("You are an individual ??? kill zombies");
+    Town.newInstance().startQuest(player);
+  }
+
+  public static void main(String args[]){
+    Game game = new Game();
+    game.play();tance()
