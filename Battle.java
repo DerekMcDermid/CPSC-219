4f7729d1ebab61515b94a304b859446384a2b5db
AnaPer <47673830+AnaPer@users.noreply.github.com>
2019-03-08 18:16:51 -0700

    Create Battle.java

---
+++ Battle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public final class Battle {
+
+    public Battle(Player player, Zombie zombie) throws IOException {
+        System.out.println("You encounter " + zombie + ": " + "\n");
+        System.out.println("Battle with " + zombie + " starts (" + player.getStatus() + " / "
+                + zombie.getStatus() + ")");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (player.isAlive() && zombie.isAlive()) {
+            System.out.print("Attack (a) or heal (h)? \n\n");
+            String action = in.readLine();
+            if (action.equals("h")) {
+                player.heal();
+            } else {
+                zombie.defend(player);
+            }
+            if (zombie.isAlive()) {
+                player.defend(zombie);
+            }
+        }
+    }
+
+}
